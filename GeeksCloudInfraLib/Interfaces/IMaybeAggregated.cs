07996b58d66e2e43cc08bfa2e694cee9a9bb5c5d
peymanebrahimi <peyman.ebrahimi@gmail.com>
2021-03-04 19:01:44 +0330

    add comment for aggregation.

--- GeeksCloudInfraLib/Interfaces/IMaybeAggregated.cs
+++ GeeksCloudInfraLib/Interfaces/IMaybeAggregated.cs
@@ -1,5 +1,9 @@
 ﻿namespace GeeksCloudInfraLib.Interfaces
 {
+    /// <summary>
+    /// Implement this interface whenever there is an aggregation relationship.
+    /// The HardDisk for vm as an example.
+    /// </summary>
     public interface IMaybeAggregated
     {
 
